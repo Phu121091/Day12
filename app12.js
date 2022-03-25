2c3d2c639e93a427813ac92dd23217a020fa0309
Phu121091 <100426997+Phu121091@users.noreply.github.com>
2022-03-25 10:38:51 +0700

    Update app12.js

--- app12.js
+++ app12.js
@@ -14,7 +14,7 @@ send.onclick = () => {
   imes.classList.add("spanright");
   imes.innerHTML = `${ochat.value}<br>`;
   chat.appendChild(imes);
-  if ((ochat.value).toLowerCase() == "hello") {
+  if (((ochat.value).toLowerCase()).trim() == "hello") {
     let smes = document.createElement("span");
     smes.innerHTML = `<img src="https://img.jamja.vn/jamja-prod/gcs_full_5abf932c76ec572ec86d0f4f-2018-03-31-135453.jpeg?cache=1">Xin chào và cảm ơn b đã liên hệ với chúng tôi .Chúng tôi có thể giúp gì cho bạn?<br>`;
     chat.appendChild(smes);
